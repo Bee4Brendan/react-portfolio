944a2892f44e0872476e598624058dfd2fef7443
Brendan Bryan <bbryan1294@gmail.com>
2023-04-22 12:39:00 -0500

    First commit

---
+++ src/sections/contact/Contact.jsx
@@ -0,0 +1,9 @@
+import './contact.css';
+
+const Contact = () => {
+  return (
+    <section id="contact"><h2>Contact</h2></section>
+  )
+}
+
+export default Contact
---
+++ src/sections/faqs/FAQs.jsx
@@ -0,0 +1,9 @@
+import './faqs.css';
+
+const FAQs = () => {
+  return (
+    <section id="faqs"><h2>FAQS</h2></section>
+  )
+}
+
+export default FAQs
