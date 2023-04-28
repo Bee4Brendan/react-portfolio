834015c7323f736dc5e995cc2412e548a0de97f7
Brendan Bryan <bbryan1294@gmail.com>
2023-04-28 17:01:10 -0500

    Added Navbar, renamed logo jpg

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
+import Logo from '../../assets/logo.jpg'
+import data from './data'
+import {IoIosColorPalette} from 'react-icons/io'
 import './navbar.css'
 
 const Navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className="container nav__container">
+        <a href="index.html" className='nav__logo'>
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className='nav__menu'>
+          {
+            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+        </ul>
+        <button id='theme__icon'><IoIosColorPalette/></button>
+      </div>
+    </nav>
   )
 }
 
