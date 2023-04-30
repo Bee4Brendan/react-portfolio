78e7efc3a5590e27cb9ddb6866e503edf8ef7fc5
Brendan Bryan <bbryan1294@gmail.com>
2023-04-30 03:31:54 +0100

    Added Header Section with Social Media Links

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,29 @@
+import HeaderImage from '../../assets/selfie.jpg'
+import data from './data'
 import './header.css'
 
 const Header = () => {
   return (
-    <header id="header"><h2>Header</h2></header>
+    <header id="header">
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Portait" />
+        </div>
+        <h2>Brendan Bryan</h2>
+        <p>
+        You have entered the Bee Hive... where you may take a glimpse through the window of my story. 
+        </p>
+        <div className="header__cta">
+          <a href="#contact" className='btn primary'>Let's Talk</a>
+          <a href="#portfolio" className='btn light'>My Work</a>
+        </div>
+        <div className="header__socials">
+          {
+            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
+          }
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,13 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineYoutube} from 'react-icons/ai'
+import {AiOutlineGithub} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+
+const data = [
+    {id: 1, link: 'https://instagram.com/bee4brendan', icon: <AiOutlineInstagram/>},
+    {id: 2, link: 'https://www.youtube.com/channel/UCjOUr41Ctn1Sa0fxdd2ne5A', icon: <AiOutlineYoutube/>},
+    {id: 3, link: 'https://github.com/Bee4Brendan', icon: <AiOutlineGithub/>},
+    {id: 4, link: 'https://www.linkedin.com/in/brendan-bryan-20b888a0/', icon: <AiOutlineLinkedin/>}
+]
+
+export default data
