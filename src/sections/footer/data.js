337a62b6530dd79f63588ef7b24f4262f614a959
Brendan Bryan <bbryan1294@gmail.com>
2023-09-26 18:39:07 -0500

    Added the Footer link

--- src/sections/footer/data.js
+++ src/sections/footer/data.js
@@ -16,5 +16,5 @@ export const socials = [
     {id: 1, link: 'https://instagram.com/Bee4Brendan', icon: <AiOutlineInstagram/>},
     {id: 2, link: 'https://www.youtube.com/channel/UCjOUr41Ctn1Sa0fxdd2ne5A', icon: <AiOutlineYoutube/>},
     {id: 3, link: 'https://github.com/Bee4Brendan', icon: <AiOutlineGithub/>},
-    {id: 4, link: 'https://www.linkedin.com/in/Brendan-Bryan-20b888a0/', icon: <AiOutlineLinkedin/>}
+    {id: 4, link: 'https://www.linkedin.com/in/Bee4Brendan', icon: <AiOutlineLinkedin/>}
 ]
