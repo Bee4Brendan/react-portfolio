fd3e9057646bc4bfa97db66bd60f639b91f13194
Brendan Bryan <bbryan1294@gmail.com>
2023-10-26 18:33:00 -0500

    Added pixel art buttons [C# and a Bee]

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import HeaderImage from '../../assets/selfie.jpg'
+import cSharpImage from '../../assets/csharp.png'
+import BeeImage from '../../assets/bee.png'
 import data from './data'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
@@ -10,6 +12,8 @@ const Header = () => {
     AOS.init({duration: 2000})
   }, [])
 
+  var link = 'https://www.freecodecamp.org/certification/Bee4Brendan/foundational-c-sharp-with-microsoft?fbclid=IwAR0Zb5i7qaop8ClE_n6NIGNLPfQURjMphLxWBQ0LqKS-EbqZ-isyTl2vf_Q';
+
   return (
     <header id="header">
       <div className="container header__container">
@@ -17,12 +21,11 @@ const Header = () => {
           <img src={HeaderImage} alt="Header Portait" title="I'm not shady, the sun was in my eyes" />
         </div>
         <h3 data-aos="fade-up">Brendan Bryan</h3>
-        <p data-aos="fade-up">
-        I'm not a typical Software Developer. As a Professional Computer Scientist <br/> 
-        with a passion for art, I am constantly striving to excel at melding those <br/>
-        two worlds. Coding and design are my forte, always with an Eye for detail. <br/><br/>
-        .NET Developers <a href="https://www.freecodecamp.org/certification/Bee4Brendan/foundational-c-sharp-with-microsoft?fbclid=IwAR0Zb5i7qaop8ClE_n6NIGNLPfQURjMphLxWBQ0LqKS-EbqZ-isyTl2vf_Q" target="_blank"><h3 className="link">C#</h3></a> "See Sharp"
-        </p>
+          <p data-aos="fade-up">
+          I'm not a typical Software Developer. As a Professional Computer Scientist <br/> 
+          with a passion for art, I am constantly striving to excel at melding those <br/>
+          two worlds. Coding and design are my forte, always with an Eye for detail. <br/><br/>
+          </p>
         <div className="header__cta" data-aos="fade-up">
           <a href="#contact" className='btn primary'>Let's Talk</a>
           <a href="#portfolio" className='btn light'>My Work</a>
@@ -34,6 +37,10 @@ const Header = () => {
           {
             data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
           }
+        </div>
+        <div>
+        <a className="btn skill_badge" href={link} target="_blank" rel="noopener noreferrer"><img src={cSharpImage} alt="Header Badge"/></a>
+        <a className="btn skill_badge" href={link} target="_blank" rel="noopener noreferrer"><img src={BeeImage} alt="Header Badge"/></a>
         </div>
       </div>
     </header>
