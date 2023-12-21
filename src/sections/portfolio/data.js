8342df71bc0c99a97cdf8af114394cba04aa4e03
Brendan Bryan <bbryan1294@gmail.com>
2023-12-21 00:33:24 -0600

    Added Thunder LINQ app to projects portfolio and incrementally updated image names

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -1,16 +1,26 @@
-import Image1 from '../../assets/project1.jpg'
+import Image1 from '../../assets/project1.png'
 import Image2 from '../../assets/project2.jpg'
 import Image3 from '../../assets/project3.jpg'
 import Image4 from '../../assets/project4.jpg'
 import Image5 from '../../assets/project5.jpg'
 import Image6 from '../../assets/project6.jpg'
 import Image7 from '../../assets/project7.jpg'
+import Image8 from '../../assets/project8.jpg'
 
 const data = [
     {
         id: 1,
-        category: 'Android Game',
+        category: 'Mobile App',
         image: Image1,
+        title: "OKC Thunder LINQ",
+        desc: "This is a C# .NET MAUI Demo for a cross-platform mobile application. It lists and sorts through OKC Thunder Players. MVVM, SOLID, LINQ queries.",
+        demo: 'https://youtu.be/tA8sAlbE0xc?si=6BGvRpIBa9NLk9fK',
+        github: 'https://github.com/Bee4Brendan/ThunderLINQ-MAUI'
+    },
+    {
+        id: 2,
+        category: 'Mobile Game',
+        image: Image2,
         title: "Dungeon Crawler",
         desc: "Mobile Android dungeon crawler game. For this game, I designed the inventory system, the items, and the GUI for the character. This includes frontend and implentation.",
         demo: 'https://youtu.be/Mazv3WOq-7c?list=PL-HMEiZRDW4Vm1grUyyJMs4uJvPVDgihv&t=782',
@@ -18,7 +28,7 @@ const data = [
     },
     {
         id: 3,
-        category: 'Android Utility',
+        category: 'Mobile App',
         image: Image3,
         title: "Contacts App",
         desc: "This is a Contacts app created for Android. It can launch emails and deliver notifications to direct the user to UCO department websites as well as store important user information.",
@@ -26,45 +36,45 @@ const data = [
         github: 'https://github.com/bee4brendan'
     },
     {
-        id: 2,
-        category: 'Android Game',
-        image: Image2,
+        id: 4,
+        category: 'Mobile Game',
+        image: Image4,
         title: "Cannot Stop",
         desc: "Android Mobile Application designed to play like the Doodle Jump game. I designed the Highscores + Main section as well as implementing the logic for gyroscope and accelerometer sensors.",
         demo: 'https://youtu.be/aJlRW58MZUw?t=136',
         github: 'https://github.com/bee4brendan'
     },
     {
-        id: 4,
-        category: 'Android Utility',
-        image: Image4,
+        id: 5,
+        category: 'Mobile App',
+        image: Image5,
         title: "Weather App",
         desc: "This Android Mobile Application allows the user to search for different cities to see the current weather for the area. There are also buttons to launch a map of the city!",
         demo: 'https://youtu.be/K3CiABLm3As',
         github: 'https://github.com/bee4brendan'
     },
     {
-        id: 5,
-        category: 'Android Game',
-        image: Image5,
+        id: 6,
+        category: 'Mobile Game',
+        image: Image6,
         title: "Baseball Number Game",
         desc: "This is an interesting game called Baseball, where the user is supposed to guess the numbers in the given 'Key'. Kind of like Wordle with numbers!",
         demo: 'https://youtu.be/fLsn16twFJw',
         github: 'https://github.com/bee4brendan'
     },
     {
-        id: 6,
+        id: 7,
         category: 'Website',
-        image: Image6,
+        image: Image7,
         title: "Website Portfolio",
         desc: "This is a website portfolio built from the ground up, showcasing my personal and professional achievements in Computer Science.",
         demo: 'https://youtu.be/aCnGlp8CtFM',
         github: 'https://github.com/Bee4Brendan/react-portfolio'
     },
     {
-        id: 7,
+        id: 8,
         category: 'Robotics',
-        image: Image7,
+        image: Image8,
         title: "Lego Robot",
         desc: "This is a robot that we programmed to follow certain directions. One of my parts was basically programming a Roomba before Roombas.",
         demo: 'https://youtu.be/tjNV6xjEO6g?t=58',
