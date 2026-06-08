ebca73907e2cf24e95f4e25089cbea7349556392
Brendan <bbryan1294@gmail.com>
2026-06-08 09:51:39 -0500

    Updated LinkedIn link

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -4,10 +4,10 @@ import {AiOutlineGithub} from 'react-icons/ai'
 import {AiOutlineLinkedin} from 'react-icons/ai'
 
 const data = [
-    {id: 1, link: 'https://instagram.com/Bee4Brendan', icon: <AiOutlineInstagram/>},
-    {id: 2, link: 'https://www.youtube.com/channel/UCjOUr41Ctn1Sa0fxdd2ne5A', icon: <AiOutlineYoutube/>},
-    {id: 3, link: 'https://github.com/Bee4Brendan', icon: <AiOutlineGithub/>},
-    {id: 4, link: 'https://www.linkedin.com/in/Bee4Brendan', icon: <AiOutlineLinkedin/>}
-]
+    { id: 1, link: "https://instagram.com/Bee4Brendan", icon: <AiOutlineInstagram /> },
+    { id: 2, link: "https://www.youtube.com/channel/UCjOUr41Ctn1Sa0fxdd2ne5A", icon: <AiOutlineYoutube /> },
+    { id: 3, link: "https://github.com/Bee4Brendan", icon: <AiOutlineGithub /> },
+    { id: 4, link: "https://www.linkedin.com/in/brendanb7539", icon: <AiOutlineLinkedin /> },
+];
 
 export default data
